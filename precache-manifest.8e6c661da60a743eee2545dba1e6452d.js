a56641646c582dcf8a7761b89b1817d25a7ff305
Ivan Bagić <ivanbagic89@gmail.com>
2020-05-31 23:12:06 +0200

    Updates

---
+++ precache-manifest.8e6c661da60a743eee2545dba1e6452d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d215df8d7a24bc31842396677ce3934b",
+    "url": "/premiere-league/index.html"
+  },
+  {
+    "revision": "364de53b1b601f1a1aa8",
+    "url": "/premiere-league/static/css/main.923c50e5.chunk.css"
+  },
+  {
+    "revision": "97ff42a62a0a38ac6ed1",
+    "url": "/premiere-league/static/js/2.99dee35d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/premiere-league/static/js/2.99dee35d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "364de53b1b601f1a1aa8",
+    "url": "/premiere-league/static/js/main.c9233242.chunk.js"
+  },
+  {
+    "revision": "dd05ce733c757376b56d",
+    "url": "/premiere-league/static/js/runtime-main.a1a64316.js"
+  },
+  {
+    "revision": "c092a7e7357324acadc6714dbb0a67e2",
+    "url": "/premiere-league/static/media/logo.c092a7e7.png"
+  }
+]);
